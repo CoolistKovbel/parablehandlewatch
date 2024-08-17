2f6ffeae638710b647c9490198ca8b83f664fd29
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-08-17 15:04:33 +0300

    config

--- app/lib/web3Cont.ts
+++ app/lib/web3Cont.ts
@@ -1,4 +1,5 @@
-import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/IUniswapV3Pool.json";
+import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+
 import { ethers } from "ethers";
 import {
   CurrencyAmount,
